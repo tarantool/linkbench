b1be5f166fdce256c922aeddb48106416a7b348b
Roman Tsisyk <roman@tarantool.org>
2017-05-16 10:33:12 +0300

    Add a Makefile to deploy MyRocks instance

--- tarantool/app.lua
+++ tarantool/app.lua
@@ -1,13 +1,11 @@
 #!/usr/bin/tarantool
 
--- Clean old data
-os.execute('rm -rf 51* 0* *.xlog *.snap *.vylog')
-
 box.cfg{
-    listen=3301,
-    log_level=4,
-    log='tarantool.log',
-    vinyl_cache = 2*1024*1024*1024
+    listen=3301;
+    log_level=4;
+    log='tarantool.log';
+    vinyl_memory = 2*1024*1024*1024;
+    vinyl_cache = 2*1024*1024*1024;
 }
 
 local engine='vinyl'
